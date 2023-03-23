73a1ad99683c86faf99ee7b27b74ca58a95aed03
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-23 09:55:15 +0100

    commit

--- menu.py
+++ menu.py
@@ -4,3 +4,12 @@ import helpers
 import database as db
 
 def menu():
+    while True:
+        helpers.limpiar_pantalla()
+        print("·································")
+        print("·········MENÚ DE PUNTOS··········")
+        print("·································")
+        print("        1. Crear punto")
+        print("        2. Calcular cuadrante")
+        print("        3. Calcular vector")
+        print("        4. Calcular distancia")
