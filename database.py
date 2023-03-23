0b4cb15f95f29a43d69d08a15fee9f4e174a09b6
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-23 08:48:30 +0100

    commit

---
+++ database.py
@@ -0,0 +1,22 @@
+import math
+
+class Punto:
+    def __init__(self, x, y):
+        self.x=x
+        self.y=y
+    def __str__(self):
+        return "Has creado el punto ({0}, {1})".format(self.x, self.y)
+    def cuadrante(self):
+        if self.x>0 & self.y>0:
+            return "El punto ({0}, {1}) está en el primer cuadrante"
+        elif self.x<0 & self.y>0:
+            return "El punto ({0}, {1}) está en el segundo cuadrante"
+        elif self.x<0 % self.y<0:
+            return "El punto ({0}, {1}) está en el tercer cuadrante"
+        else:
+            return "El punto ({0}, {1}) está en el cuarto cuadrante"
+    
+    def vector(self, punto):
+        return Punto(punto.x-self.x, punto.y-self.y)
+    
+    
