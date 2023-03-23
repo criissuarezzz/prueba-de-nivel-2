2ade3f5a602c067e0a2384bafa84c94c14249390
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-23 09:55:54 +0100

    commit

--- menu.py
+++ menu.py
@@ -13,3 +13,5 @@ def menu():
         print("        2. Calcular cuadrante")
         print("        3. Calcular vector")
         print("        4. Calcular distancia")
+        print("        5. Crear rectángulo")
+        print("        6. Dibujar rectángulo")
