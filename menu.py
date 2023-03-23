23be6ba16cbb1e283543a81d77c3658e489b0181
criissuarezzz <cristina.suarezlimon@usp.ceu.es>
2023-03-23 10:00:17 +0100

    commit

--- menu.py
+++ menu.py
@@ -10,7 +10,7 @@ def menu():
         print("·········MENÚ DE PUNTOS··········")
         print("·································")
         print("        1. Crear punto")
-        print("        2. Calcular cuadrante")
+        print("        2. Consultar cuadrante")
         print("        3. Calcular vector")
         print("        4. Calcular distancia")
         print("        5. Crear rectángulo")
@@ -20,5 +20,21 @@ def menu():
 
         opcion = input("Elige una opción: ")
         helpers.limpiar_pantalla()
-        if opcion == 1:
+        if opcion == "1":
+            print("Has elegido la opción 1, crear punto")
+            op=input("¿desea continuar?(s/n)")
+            if op == "s":
+                db.Punto_menu.crear_punto()
+            else:
+                print("Has elegido no continuar")
+        
+        elif opcion=="2":
+            print("Has elegido la opción 2, consultar cuadrante")
+            op=input("¿desea continuar?(s/n)")
+            if op == "s":
+                db.Punto_menu.cuadrante()
+            else:
+                print("Has elegido no continuar")
+        
+        elif opcion=="3":
             
